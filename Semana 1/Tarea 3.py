7be876fe37a5224fee8ba6e642a4c3b531130ca0
PC-4FK <dietreinfa@gmail.com>
2025-01-25 16:28:06 -0600

    Create "Tarea 3"

---
+++ Semana 1/Tarea 3.py
@@ -0,0 +1,48 @@
+'''
+Ejercicio 1-Diego Alberto Infante Treviño-257510
+
+1-¿Cúal es la función de la memoria secundaria en una computadora? c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado
+  y encendido.
+
+2-¿Que es un programa? c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado
+y encendido.
+
+3-¿Cuál es la diferencia entre un compilador y un intérprete? Un intérprete consiste en ser capaz de mantener una conversación
+ interactiva y un compilador necesita que le entreguen el programa completo en un fichero, y luego ejecuta un proceso para traducir el
+ código fuente de alto nivel a código máquina, tras lo cual coloca ese código máquina
+ resultante dentro de otro fichero para su ejecución posterior.
+
+4-¿Cuál de los siguientes contiene “código máquina”? b) El teclado
+
+5-¿Qué está mal en el siguiente código?
+>>> primt ’¡Hola, mundo!’
+File "<stdin>", line 1
+primt ’¡Hola, mundo!’
+^
+SyntaxError: invalid syntax
+>>>
+Respuesta: la palabra print esta mal escrita y le hace falta ponerle los parentesis
+
+6-¿En qué lugar del computador queda almacenada una variable, como en este caso “x”,
+después de ejecutar la siguiente línea de Python?:
+x = 123
+Respuesta: b)Memoria principal
+
+7-¿Qué mostrará en pantalla el siguiente programa?:
+x = 43
+x = x + 1
+print(x)
+Respuesta: b)44 porque el la variable ya esta definida como la suma del numero que inciaste, si esstuviera alreves se mostraria el 43
+
+8-Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad
+humana-
+1. Unidad central de procesamiento:
+2. Memoria principal:
+3. Memoria secundaria:
+4. Dispositivos de entrada:
+5. Dispositivos de salida:
+
+9-¿Cómo puedes corregir un “Error de sintaxis”?
+
+
+'''
