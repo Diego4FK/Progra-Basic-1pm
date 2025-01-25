12df75838f7f389a07e97a7b552428a97b192857
PC-4FK <dietreinfa@gmail.com>
2025-01-24 19:00:59 -0600

    Create "ACT-1"

---
+++ Semana 1/ACT-1.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 1
+1-¿Cúal es la función de la memoria secundaria en una computadora?
+  c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado
+  y encendido.
+2-¿Que es un programa? c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado
+y encendido.
+3-¿Cuál es la diferencia entre un compilador y un intérprete?   
+
+'''
